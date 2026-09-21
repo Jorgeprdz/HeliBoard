3215d15e0f574dc4fcf89071c53905df59684b26
Jorgeprdz <JORGEPALACIOSRODRIGUEZ@GMAIL.COM>
2026-09-21 17:51:13 -0600

    cleanup: remove unused GIF module (GifSelection.kt)

--- app/src/main/java/helium314/keyboard/keyboard/gif/GifSelection.kt
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-only
-package helium314.keyboard.keyboard.gif
-
-data class GifSelection(
-    val id: String,
-    val title: String,
-    val mediaUrl: String,
-    val pageUrl: String?
-)
