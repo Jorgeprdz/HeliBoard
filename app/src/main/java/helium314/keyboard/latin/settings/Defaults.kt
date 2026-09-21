edddf459ad05d09cfe030882aa7111e8b0b678dd
Jorgeprdz <JORGEPALACIOSRODRIGUEZ@GMAIL.COM>
2026-09-21 17:52:38 -0600

    cleanup: remove unused default language subtypes

--- app/src/main/java/helium314/keyboard/latin/settings/Defaults.kt
+++ app/src/main/java/helium314/keyboard/latin/settings/Defaults.kt
@@ -80,9 +80,7 @@ object Defaults {
     const val PREF_SHOW_EMOJI_KEY = false
     const val PREF_SHOW_DPAD_KEY = true
     const val PREF_VARIABLE_TOOLBAR_DIRECTION = true
-    const val PREF_ADDITIONAL_SUBTYPES = "de${Separators.SET}${ExtraValue.KEYBOARD_LAYOUT_SET}=MAIN:qwerty${Separators.SETS}" +
-            "fr${Separators.SET}${ExtraValue.KEYBOARD_LAYOUT_SET}=MAIN:qwertz${Separators.SETS}" +
-            "hu${Separators.SET}${ExtraValue.KEYBOARD_LAYOUT_SET}=MAIN:qwerty"
+    const val PREF_ADDITIONAL_SUBTYPES = ""
     const val PREF_ENABLE_SPLIT_KEYBOARD = false
     @JvmField
     val PREF_SPLIT_SPACER_SCALE = Array(4) { DEFAULT_SIZE_SCALE }
